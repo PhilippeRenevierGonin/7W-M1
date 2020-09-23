4d6feb51f21ec20e8518c3c1404ce87c270edaba
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2020-09-23 15:03:11 +0200

    #2 mise en place de maven

---
+++ src/main/java/sevenw/app/SevenWonders.java
@@ -0,0 +1,10 @@
+package sevenw.app;
+
+public class SevenWonders {
+
+    public final static void main(String [] args) {
+        System.out.println("**** 7 wonders ****");
+        System.out.println("**** fin ****");
+
+    }
+}
